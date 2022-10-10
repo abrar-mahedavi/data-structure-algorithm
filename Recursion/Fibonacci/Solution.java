3ef8ff8090fab1dd767ac9f168bf0c4451352318
abrar-mahedavi <mahedavi.abrar@gmail.com>
2022-10-10 20:01:55 +0530

    Recursion practice fibonacci.
    - Add code for fibonacci.
    - Add the test cases for fibonacci.

---
+++ Recursion/Fibonacci/Solution.java
@@ -0,0 +1,19 @@
+package com.passbook.sparkeighteen.recursion;
+
+public class Solution {
+
+    public static Integer fibonacci(Integer index) {
+        if (index == 1 || index == 2) {
+            return 1;
+        } else {
+            return fibonacci(index - 1) + fibonacci(index - 2);
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 1; i < 10; i++) {
+            System.out.println(fibonacci(i));
+        }
+
+    }
+}
